19c9a072f44aed6bf5c7b31ccbbdffe171d6c523
Ilya Sychev <58861662+bipolaroc4ka@users.noreply.github.com>
2025-05-04 19:38:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,213 @@
-// Замените URL на ваш URL из Google Apps Script Web App
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz9JXMxygYFUVi_UUdK75GN8dYbpBzfsl_z8fGpSrCw2ItD6PUr4ZXqj3VW1D0Ib3koJQ/exec';
-const form = document.getElementById('myForm'); // Получаем элемент формы по ID
+// импортируем слова из файла
+import { WORDS } from "words.js";
 
-const statusDiv = document.getElementById('status');
+// количество попыток
+const NUMBER_OF_GUESSES = 6;
+// сколько попыток осталось
+let guessesRemaining = NUMBER_OF_GUESSES;
+// текущая попытка
+let currentGuess = [];
+// следующая буква
+let nextLetter = 0;
+// загаданное слово
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+// на всякий случай выведем в консоль загаданное слово, чтобы проверить, как работает игра
+console.log(rightGuessString)
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
+// создаём игровое поле
+function initBoard() {
+    // получаем доступ к блоку на странице
+    let board = document.getElementById("game-board");
 
-  // Собираем данные из формы
-  const formData = new FormData(form);
-  const name = formData.get('name');
-  const score = formData.get('score'); // предполагается, что есть поле score в форме
+    // создаём строки
+    // делаем цикл от 1 до 6, потому что попыток у нас как раз 6
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+        // создаём новый блок на странице
+        let row = document.createElement("div")
+        // добавляем к нему класс, чтобы потом работать со строками напрямую
+        row.className = "letter-row"
+        
+        // создаём отдельные клетки
+        // добавляем по 5 клеток в ряд
+        for (let j = 0; j < 5; j++) {
+            // создаём новый блок на странице
+            let box = document.createElement("div")
+            // добавляем к нему класс
+            box.className = "letter-box"
+            // вкладываем новый блок внутрь блока со строкой
+            row.appendChild(box)
+        }
 
-  // Отправляем данные в нужном формате
-  fetch(scriptURL, {
-    method: 'POST',
-    body: JSON.stringify({ name, score }),
-    headers: { 'Content-Type': 'application/json' }
-  })
-  .then(response => {
-    statusDiv.innerHTML = 'Сообщение отправлено!';
-    form.reset();
-  })
-  .catch(error => {
-    statusDiv.innerHTML = 'Ошибка отправки. Попробуйте ещё раз.';
-  });
-});
-function getScores() {
-  fetch(scriptURL)
-    .then(res => res.json())
-    .then(data => {
-      statusDiv.innerHTML = '<h3>Таблица рекордов:</h3>';
-      data.forEach(([name, score]) => {
-        statusDiv.innerHTML += `<p>${name}: ${score}</p>`;
-      });
-    });
+        // как все 5 клеток готовы, добавляем новую строку на поле
+        board.appendChild(row)
+    }
 }
 
-// Например, вызываем отправку
-// sendScore('bipolaroc4ka', 300);
-getScores();
+// удаление символа
+function deleteLetter () {
+    // получаем доступ к текущей строке
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    // и к последнему введённому символу
+    let box = row.children[nextLetter - 1]
+    // очищаем содержимое клетки
+    box.textContent = ""
+    // убираем жирную обводку
+    box.classList.remove("filled-box")
+    // удаляем последний символ из массива с нашей текущей догадкой
+    currentGuess.pop()
+    // помечаем, что у нас теперь на одну свободную клетку больше
+    nextLetter -= 1
+}
+
+// проверка введённого слова
+function checkGuess () {
+    // получаем доступ к текущей строке
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    // переменная, где будет наша догадка
+    let guessString = ''
+    // делаем из загаданного слова массив символов
+    let rightGuess = Array.from(rightGuessString)
+
+    // собираем все введённые в строке буквы в одно слово
+    for (const val of currentGuess) {
+        guessString += val
+    }
+
+    // если в догадке меньше 5 букв — выводим уведомление, что букв не хватает
+    if (guessString.length != 5) {
+        // error означает, что уведомление будет в формате ошибки
+        toastr.error("Введены не все буквы!");
+        // и после вывода выходим из проверки догадки
+        return;
+    }
+
+    // если введённого слова нет в списке возможных слов — выводим уведомление
+    if (!WORDS.includes(guessString)) {
+        toastr.error("Такого слова нет в списке!")
+        // и после вывода выходим из проверки догадки
+        return;
+    }
+
+    // перебираем все буквы в строке, чтобы подсветить их нужным цветом
+    for (let i = 0; i < 5; i++) {
+        // убираем текущий цвет, если он был
+        let letterColor = ''
+        // получаем доступ к текущей клетке
+        let box = row.children[i]
+        // и к текущей букве в догадке
+        let letter = currentGuess[i]
+        
+        // смотрим, на каком месте в исходном слове стоит текущая буква
+        let letterPosition = rightGuess.indexOf(currentGuess[i])
+        // если такой буквы нет в исходном слове
+        if (letterPosition === -1) {
+            // закрашиваем клетку серым
+            letterColor = 'grey'
+        // иначе, когда мы точно знаем, что буква в слове есть
+        } else {
+            // если позиция в слове совпадает с текущей позицией 
+            if (currentGuess[i] === rightGuess[i]) {
+                // закрашиваем клетку зелёным
+                letterColor = 'green'
+            } else {
+                // в противном случае закрашиваем жёлтым
+                letterColor = 'yellow'
+            }
+
+            // заменяем обработанный символ на знак решётки, чтобы не использовать его на следующем шаге цикла
+            rightGuess[letterPosition] = "#"
+        }
+
+        // применяем выбранный цвет к фону клетки
+        box.style.backgroundColor = letterColor;
+    }
+
+    // если мы угадали
+    if (guessString === rightGuessString) {
+        // выводим сообщение об успехе
+        toastr.success("Вы выиграли!")
+        // обнуляем количество попыток
+        guessesRemaining = 0;
+        // выходим из проверки
+        return
+    // в противном случае
+    } else {
+        // уменьшаем счётчик попыток
+        guessesRemaining -= 1;
+        // обнуляем массив с символами текущей попытки
+        currentGuess = [];
+        // начинаем отсчёт букв заново
+        nextLetter = 0;
+
+        // если попытки закончились
+        if (guessesRemaining === 0) {
+            // выводим сообщение о проигрыше
+            toastr.error("У вас не осталось попыток. Вы проиграли!");
+            // и выводим загаданное слово
+            toastr.info(`Загаданное слово: "${rightGuessString}"`)
+        }
+    }
+}
+
+// выводим букву в клетку
+function insertLetter (pressedKey) {
+    // если клетки закончились
+    if (nextLetter === 5) {
+        // выходим из функции
+        return;
+    }
+    // получаем доступ к текущей строке
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    // и к текущей клетке, где будет появляться буква
+    let box = row.children[nextLetter]
+    // меняем текст в блоке с клеткой на нажатый символ
+    box.textContent = pressedKey
+    // добавляем к клетке жирную обводку
+    box.classList.add("filled-box")
+    // добавляем введённый символ к массиву, в которой хранится наша текущая попытка угадать слово
+    currentGuess.push(pressedKey)
+    // помечаем, что дальше будем работать со следующей клеткой
+    nextLetter += 1
+}
+
+// обработчик нажатия на клавиши
+document.addEventListener("keydown", (e) => {
+
+    // если попыток не осталось
+    if (guessesRemaining === 0) {
+        // выходим из функции
+        return
+    }
+
+    // получаем код нажатой клавиши
+    let pressedKey = String(e.key)
+    // если нажат Backspace и в строке есть хоть один символ
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
+        // то удаляем последнюю введённую букву
+        deleteLetter();
+        // и выходим из обработчика
+        return;
+    }
+
+    // если нажат Enter
+    if (pressedKey === "Enter") {
+        // проверяем введённое слово
+        checkGuess();
+        // и выходим из обработчика
+        return;
+    }
+
+    // проверяем, есть ли введённый символ в английском алфавите
+    let found = pressedKey.match(/[a-z]/gi)
+    // если нет
+    if (!found || found.length > 1) {
+        // то выходим из обработчика
+        return
+    // иначе добавляем введённую букву в новую клетку
+    } else {
+        insertLetter(pressedKey)
+    }
+})
 
 
+initBoard();
