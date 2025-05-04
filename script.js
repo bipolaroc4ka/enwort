7e1f30dad56ed8858b21fbde58c532ac4694d461
Ilya Sychev <58861662+bipolaroc4ka@users.noreply.github.com>
2025-05-04 18:59:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
 // Замените URL на ваш URL из Google Apps Script Web App
 const scriptURL = 'https://script.google.com/macros/s/AKfycbysfmelCyvHgZj3CVXKdhgM2Z8-529wi-rvcWUriLAjt1uuhI5apqi2Q_Dn4t1kYvH4pg/exec';
 
-const form = document.getElementById('myForm');
+
 const statusDiv = document.getElementById('status');
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-      statusDiv.innerHTML = 'Сообщение отправлено!';
-      form.reset();
-    })
-    .catch(error => {
-      statusDiv.innerHTML = 'Ошибка отправки. Попробуйте ещё раз.';
-    });
-});
+function sendScore(name, score) {
+  fetch(scriptURL, {
+    method: 'POST',
+    body: JSON.stringify({ name, score }),
+    headers: { 'Content-Type': 'application/json' }
+  }).then(() => getScores());
+}
 
-function getData() {
+function getScores() {
   fetch(scriptURL)
     .then(res => res.json())
     .then(data => {
-      console.log(data); // Посмотрим что вернёт
-      statusDiv.innerHTML = '';
-      data.forEach(row => {
-        statusDiv.innerHTML += `<p>${row.join(' | ')}</p>`;
+      statusDiv.innerHTML = '<h3>Таблица рекордов:</h3>';
+      data.forEach(([name, score]) => {
+        statusDiv.innerHTML += `<p>${name}: ${score}</p>`;
       });
-    })
-    .catch(err => {
-      statusDiv.innerHTML = 'Ошибка получения данных';
     });
 }
 
-// Вызовем чтение сразу при открытии сайта
-getData();
+// Например, вызываем отправку
+// sendScore('bipolaroc4ka', 300);
+getScores();
+
 
